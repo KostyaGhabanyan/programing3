3810a1663577b141862a037cc64959b415574271
Your Name <you@example.com>
2018-05-14 17:33:53 +0400

    glux caval

---
+++ Lesson5/script.js
@@ -0,0 +1,31 @@
+function main() {
+   var socket = io.connect('http://localhost:3000');
+   var chatDiv = document.getElementById('chat');
+   var input = document.getElementById('message');
+   var button = document.getElementById('submit');
+   var Delete = document.getElementById("Delete")
+   function handleSubmit(evt) {
+       var val = input.value;
+       if (val != "") {
+           socket.emit("send message", val);
+       }
+   }
+   function handleDelete(evt) {
+       var val = input.value;
+       if () {
+           
+       }
+   }
+   button.onclick = handleSubmit;
+   Delete.onclick = handleDelete;
+function handleMessage(msg) {
+   		var p = document.createElement('p');
+   		p.innerText = msg;
+   		chatDiv.appendChild(p);
+   		input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;
